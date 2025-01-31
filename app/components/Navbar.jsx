90ab525e7414ed14e891b9d4d868eb066e2be645
ShadaabHasan <112100669+ShadaabHasan@users.noreply.github.com>
2025-01-31 20:43:51 +0530

    navbar updates

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,15 +4,15 @@ const navbar = () => {
   return (
     <>
       <div className="relative min-h-screen flex flex-col">
-        <nav className="fixed top-0 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50 border-b border-border">
-          <div class="container mx-auto px-4">
-          <div class="flex h-16 items-center justify-between">
+        <nav className="fixed top-0 w-full border-b border-border">
+          <div className="container mx-auto px-4">
+            <div className="flex h-16 items-center justify-between">
               <a className='text-xl font-bold' href='/'>Shadaab Hasan | Portfolio </a>
               <a className="text-foreground/60 hover:text-foreground" href="/">Home</a>
               <a className="text-foreground/60 hover:text-foreground" href="/about/">About</a>
               <a className="text-foreground/60 hover:text-foreground" href="/projects/">Projects</a>
               <a className="text-foreground/60 hover:text-foreground" href="/contact/">Contact</a>
-              </div>
+            </div>
           </div>
         </nav>
       </div>
