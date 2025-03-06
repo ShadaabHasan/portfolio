1722f2136b12b7501f843cc287406d46291e5069
ShadaabHasan <112100669+ShadaabHasan@users.noreply.github.com>
2025-03-07 00:00:16 +0530

    speed insights on vercel

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 
@@ -37,6 +38,7 @@ export default function RootLayout({ children }) {
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
